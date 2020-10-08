f07dc2edfd47413b760a079a7c3d0e0c250dbc3a
Aidan <Aidan@AidanPC>
2020-10-07 19:57:26 -0700

    ewqtrewqfwqet

---
+++ src/_01_intro_to_static/IntroToStatic.java
@@ -0,0 +1,22 @@
+package _01_intro_to_static;
+
+public class IntroToStatic {
+    static int nextBibNumber;
+    static String raceLocation = "The moon";
+    static String raceStartTime = "13.00am";
+
+    String name;
+    int speed;
+    int bibNumber;
+
+   void Athlete (String name, int speed){
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public static void main(String[] args) {
+        //create two athletes
+        //print their names, bibNumbers, and the location of their race. 
+    }
+}
+
---
+++ src/_01_intro_to_static/Maker.java
@@ -0,0 +1,12 @@
+package _01_intro_to_static;
+
+public class Maker {
+public static void main(String[] args) {
+	
+
+
+Athlete troy = new Athlete(String Troy, int 1000, int 200);
+
+}
+
+}

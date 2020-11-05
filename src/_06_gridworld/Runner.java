76bfb3e97676403221abba64cce73e5cfdb57464
Aidan <Aidan@AidanPC.lan>
2020-11-04 19:58:43 -0800

    adsfasef

---
+++ src/_06_gridworld/Runner.java
@@ -0,0 +1,24 @@
+package _06_gridworld;
+
+import java.awt.Color;
+
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
+import info.gridworld.grid.Location;
+import info.gridworld.world.World;
+
+public class Runner {
+public static void main(String[] args) {
+	World world = new World();
+	world.show();
+	Bug redbug = new Bug();
+	Location loc = new Location(1,1);
+	world.add(loc, redbug);
+	Location library = new Location(2,1);
+	world.add(library, library);
+	Flower flower1 = new Flower();
+redbug.turn();
+redbug.turn();
+	
+}
+}
